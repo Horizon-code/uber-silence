6df8727e7bcb5d7dc7735acbd907d30d93832e87
Horizon-code <rubenpasamarsanchez@gmail.com>
2021-11-29 11:15:25 +0100

    adding files

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
-import React from 'react';
-import { SafeAreaView, StyleSheet, Text } from 'react-native';
-import { Provider } from 'react-redux';
-import { store } from './store';
-import HomeScreen from "./screens/HomeScreen";
-import MapScreen from "./screens/MapScreen";
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import React from "react";
+import { store } from "./store";
+import { Provider } from "react-redux";
 import "react-native-gesture-handler";
+import HomeScreen from "./screens/HomeScreen";
 import { NavigationContainer } from "@react-navigation/native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { KeyboardAvoidingView } from "react-native";
+import MapScreen from "./screens/MapScreen";
 import { createStackNavigator } from "@react-navigation/stack";
+
+// The Google API's we use in this project are:
+// https://console.cloud.google.com/google/maps-apis/api-list
+
+// - Directions API
+// - Places API
+// - Distance Matrix API
 
 export default function App() {
   const Stack = createStackNavigator();
@@ -15,33 +22,31 @@ export default function App() {
   return (
     <Provider store={store}>
       <NavigationContainer>
-      <SafeAreaProvider>
-        <Stack.Navigator>
-          <Stack.Screen
-           name='HomeScreen'
-           component={HomeScreen}
-           options={{
-            headerShown: false,
-          }}/>
-          <Stack.Screen
+        <SafeAreaProvider>
+          <KeyboardAvoidingView
+            behavior={Platform.OS === "ios" ? "padding" : "height"}
+            style={{ flex: 1 }}
+            keyboardVerticalOffset={Platform.OS === "ios" ? -64 : 0}
+          >
+            <Stack.Navigator initialRouteName="HomeScreen">
+              <Stack.Screen
+                name="HomeScreen"
+                component={HomeScreen}
+                options={{
+                  headerShown: false,
+                }}
+              />
+              <Stack.Screen
                 name="MapScreen"
                 component={MapScreen}
                 options={{
                   headerShown: false,
                 }}
               />
-        </Stack.Navigator>
-      </SafeAreaProvider>
+            </Stack.Navigator>
+          </KeyboardAvoidingView>
+        </SafeAreaProvider>
       </NavigationContainer>
     </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
